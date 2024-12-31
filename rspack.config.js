cf764ebe8992bcc0855e6fc8173f677c96621582
jadiazp <jadiazp@indracompany.com>
2024-12-30 21:51:31 -0500

    Agregar archivos de configuración para Rsbuild y Vercel, incluyendo rspack.config.js y vercel.json.

---
+++ rspack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    output: {
+      path: path.resolve(__dirname, "dist"), // La carpeta donde se generará el build
+      filename: "[name].js",
+      publicPath: "/",
+    },
+  };
